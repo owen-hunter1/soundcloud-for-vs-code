218e0af5e235c2e73474f037e1b3e4cb2ecde637
owen-hunter1 <70711604+owen-hunter1@users.noreply.github.com>
2021-03-10 12:50:51 -0500

    added track player methods

--- src/soundcloud_request.ts
+++ src/soundcloud_request.ts
@@ -3,21 +3,31 @@ import * as vscode from 'vscode';
 
 var http = require("https");
 
+function displayConnectionError(err: string){
+    vscode.window.showErrorMessage("unable to connect to soundcloud servers. Error message: \n" + err);
+}
+
 export class SoundCloudRequest{
 
     static queryTrack(query: string, callback: Function){
-        http.get("https://api-v2.soundcloud.com/search/queries?q=" + query + "&client_id=dmDh7QSlmGpzH9qQoH1YExYCGcyYeYYC&limit=1", (response: EventEmitter)=>{
+        //http get method
+        http.get("https://api-v2.soundcloud.com/search/queries?q=" + query + "&client_id=dmDh7QSlmGpzH9qQoH1YExYCGcyYeYYC&limit=5", (response: EventEmitter)=>{
             var data = '';
             response.on("data", (chunk)=>{
+                //build data from chunks
                 data += chunk;
             });
             response.on("end", ()=>{
                 var jsonObj = JSON.parse(data);
                 var strArr = [];
                 for(var i = 0; i < jsonObj.collection.length; i++){
+                    //todo: parse as track array instead of string array
                     strArr.push(jsonObj.collection[i].output);
                 }
                 callback(strArr);
+            });
+            response.on("error", (err)=>{
+                displayConnectionError(err);
             });
         });
     }
--- src/trackplayer.ts
+++ src/trackplayer.ts
@@ -3,10 +3,39 @@ export interface Track{
     artist: string,
     album: string,
     trackID: number
+    //path: string
 }
 
 export class TrackPlayer{
+    //queue is an array of Tracks. Songs appear in the array in the order which they are to be played
+    private queue: Array<Track>;
     constructor(){
-        
+        this.queue = [];
+    }
+    public addToQueue(track: Track){
+        this.queue.push(track);
+    }
+    public removeFromQueue(i: number){
+        if(this.queue.length > 0){
+            this.queue.splice(i, 1);
+        }
+    }
+    public getCurrentTrack(): Track{
+        return this.queue[0];
+    }
+    public getQueue(): Array<Track>{
+        return this.queue;
+    }
+    public play(){
+
+    }
+    public pause(){
+
+    }
+    public skipNext(){
+
+    }
+    public skipBack(){
+
     }
 }
