6567b16ffb9dab9332c735f16b6b8a8bdf3ea8b5
Paradox05 <ronielabreu22@gmail.com>
2021-03-17 19:52:19 -0400

    Update timer.ts

--- src/timer.ts
+++ src/timer.ts
@@ -1,5 +1,23 @@
-class Timer{
+export class Timer{
+    private timeRemaining: number;
     constructor(){
+        this.timeRemaining = 0;
         
     }
+
+    public tick() {
+        let timeStart = new Date().getTime();
+        return {
+            
+            get time1() {
+                const time1 = (new Date().getTime() - timeStart / 1000);
+                return time1;
+            },
+            get time2() {
+                const time2 = (new Date().getTime() - timeStart);
+                return time2;
+            }
+        }
+    }
+    
 }
